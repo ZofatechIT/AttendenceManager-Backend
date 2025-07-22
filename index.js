b041977b1cf2b09db6ede6b5979267f943054661
ZofatechIT <zofatechithub@gmail.com>
2025-07-22 15:35:12 +0500

    Update port number index.js

--- index.js
+++ index.js
@@ -551,5 +551,5 @@ app.put('/api/admin/attendance/record/:id', auth, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
